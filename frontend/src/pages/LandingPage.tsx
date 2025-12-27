f8b56446552f7e445498656b565059eba74fab60
Hektor <hektorb@umich.edu>
2025-12-26 19:46:01 -0500

    Fixed page routing for frontend

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,18 +1,21 @@
 import Button from "../components/Button";
+import { useNavigate } from "react-router-dom";
 
-const loginHandler = () => {
-  console.log("Send user to login page.\n");
-};
+// const loginHandler = () => {
+//   console.log("Send user to login page.\n");
+// };
 
-const createHandler = () => {
-  console.log("Send user to sign up page.\n");
-};
+// const createHandler = () => {
+//   console.log("Send user to sign up page.\n");
+// };
 
-const guestHandler = () => {
-  console.log("Send user to main page.\n");
-};
+// const guestHandler = () => {
+//   console.log("Send user to main page.\n");
+// };
 
 function LandingPage() {
+  const navigate = useNavigate();
+
   return (
     <>
       <h1>Welcome to Hektor's Movie Recommender</h1>
@@ -20,13 +23,13 @@ function LandingPage() {
         Sign in to view and modify your list of seen movies for a better
         recommendation. Otherwise, go through guest for a quick search!
       </p>
-      <Button color="success" onClick={loginHandler}>
+      <Button color="success" onClick={() => navigate("/login")}>
         Login
       </Button>
-      <Button color="success" onClick={createHandler}>
+      <Button color="success" onClick={() => navigate("/signup")}>
         Create Account
       </Button>
-      <Button color="success" onClick={guestHandler}>
+      <Button color="success" onClick={() => navigate("/query")}>
         Continue as Guest
       </Button>
     </>
