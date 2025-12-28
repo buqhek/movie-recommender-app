a01155513fd054a92f347cc64ee434b5a461ccfa
Hektor <hektorb@umich.edu>
2025-12-28 00:12:59 -0500

    developed app and config for backend

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,32 @@
 from flask import Flask
 from config import DevelopmentConfig
+import sqlite3
+import os
+
+def init_db(database_path):
+    """Initialize database if it doesn't exist."""
+    if not os.path.exists(database_path):
+        # Make db
+        conn = sqlite3.connect(database_path)
+        with open('data/schema.sql', 'r') as f:
+            conn.executescript(f.read())
+        conn.commit()
+        conn.close()
+        print(f"Database initialized at {database_path}")
+    else:
+        print(f"Database already exists at {database_path}")
 
 def create_app(config_class=DevelopmentConfig):
+    """Creates the flask server."""
     app = Flask(__name__)
     app.config.from_object(config_class)
     
-    return app
+    # Initialize Database
+    init_db(app.config['DATABASE_PATH'])
 
+    return app
+              
 if __name__ == '__main__':
     app = create_app()
+    
     app.run()
