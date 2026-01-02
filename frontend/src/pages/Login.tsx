256fd13b0d457c34ca87be83e37567b600b4f2ca
Hektor <hektorb@umich.edu>
2026-01-02 17:58:09 -0500

    working on frontend login API call

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,15 +1,59 @@
 import { useState, useEffect } from "react";
 import Button from "../components/Button";
 
-const loginFormHandler = () => {
-  console.log("Send login form to backend.\n");
+const [username, setUsername] = useState("");
+const [password, setPassword] = useState("");
+const [result, setResult] = useState({});
+
+interface User {
+  name: string;
+  email: string;
+}
+
+const handleUsernameInput = (event) => {
+  setUsername(event.target.value);
+};
+
+const handlePasswordInput = (event) => {
+  setPassword(event.target.value);
+};
+
+async function fetchLoginInfo(): Promise<User> {
+  const response = await fetch("/api/v1/login");
+  const data = await response.json();
+  return data;
+}
+
+useEffect(() => {
+  fetchLoginInfo().then((data) => setResult(data));
+}, []);
+
+const loginFormHandler = async () => {
+  // console.log("Send login form to backend.\n");
+  const request: RequestInfo = new Request("./api/v1/login", {
+    method = "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(User),
+  });
+
+  fetch(request)
+    .then((res) => res.json())
+    .then((res) => {
+      setResult(res); // Save the fetch result to the "result" object (a hashmap)
+      console.log(res); // Print the result
+    });
+  // Fetching the api data using async await
+  // try{
+  //   const response = await fetch('/api/v1/login'){
+  //     Crede
+  //   }
+  // }
 };
 
 function Login() {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
   return (
     <>
       <div className="form-floating mb-3">
@@ -18,6 +62,8 @@ function Login() {
           className="form-control"
           id="floatingInput"
           placeholder="unique_username"
+          onChange={handleUsernameInput} // Necessary to update the
+          // useStates during input
         />
         <label htmlFor="floatingInput">Username</label>
       </div>
@@ -28,6 +74,8 @@ function Login() {
           className="form-control"
           id="floatingPassword"
           placeholder="Password"
+          onChange={handlePasswordInput} // Necessary to update the
+          // useStates during input
         />
         <label htmlFor="floatingPassword">Password</label>
       </div>
