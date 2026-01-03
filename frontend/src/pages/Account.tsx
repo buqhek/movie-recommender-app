20e4ec8c5f79b27b50afcaef512e62d3da9d9b3e
Hektor <hektorb@umich.edu>
2026-01-02 20:20:19 -0500

    added logout button to /accout, but not working properly

--- frontend/src/pages/Account.tsx
+++ frontend/src/pages/Account.tsx
@@ -1,6 +1,36 @@
-function Account() {
-  return <h1>TODO: Build Account</h1>;
+import type { FormEvent } from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
+function Account() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogout = async (
+    event: FormEvent<HTMLFormElement>
+  ): Promise<void> => {
+    event.preventDefault();
+    setLoading(true);
+
+    try {
+      const response = await fetch("asdf", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        credentials: "include",
+      });
+
+      if (response.status == 200) {
+        navigate("/");
+      } else {
+        setError("Error: Authentiation required");
+      }
+    } catch (err) {
+      console.log("Error:", { err });
+    } finally {
+      setLoading(false);
+    }
+  };
   // TODO: Allow for username change,
   //       account deletion, and movie
   //       rating updating
@@ -11,6 +41,25 @@ function Account() {
   //       movie appear as a rating from 0-5 with 0.5
   //       increments. This can be in star format, or
   //       something for user implementation.
+  return (
+    <form
+      onSubmit={(e) => {
+        handleLogout(e);
+      }}
+    >
+      <div>
+        <button
+          type="submit"
+          color="success"
+          className="btn btn-success"
+          disabled={loading}
+        >
+          Logout
+        </button>
+        {error && <p className="button-error-text">{error}</p>}
+      </div>
+    </form>
+  );
 }
 
 export default Account;
